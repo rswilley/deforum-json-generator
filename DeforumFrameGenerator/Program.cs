e633415c70468111c4f9936df44c403e88578842
Robert Swilley <robert@rswilley.com>
2024-03-24 10:27:45 -0500

    first commit

---
+++ DeforumFrameGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using DeforumFrameGenerator;
+
+Console.WriteLine("Loading settings...");
+
+await using var stream = new FileStream("settings.json", FileMode.Open);
+var settings = await JsonSerializer.DeserializeAsync<Settings>(stream);
+
+var totalSeconds = settings!.VideoLength.TotalSeconds;
+var totalFrames = totalSeconds * settings.Fps;
+
+var segmentCount = totalSeconds / settings.SegmentLengthInSeconds;
+var frameIteration = (int)(totalFrames / segmentCount);
+
+var output = new Dictionary<string, string>();
+var frameIndex = 0;
+
+for (int i = 0; i < segmentCount; i++)
+{
+    output.Add(frameIndex.ToString(), "");
+    frameIndex += frameIteration;
+}
+
+Console.WriteLine($"Total Frames: {totalFrames + settings.Fps}");
+Console.WriteLine("Deforum JSON: " + JsonSerializer.Serialize(output, new JsonSerializerOptions
+{
+    WriteIndented = true
+}));
+Console.ReadKey();
---
+++ DeforumFrameGenerator/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DeforumFrameGenerator;
+
+public class Settings
+{
+    [JsonPropertyName("videoLength")]
+    public TimeSpan VideoLength { get; init; }
+    [JsonPropertyName("fps")]
+    public int Fps { get; init; }
+    [JsonPropertyName("segmentLengthInSeconds")]
+    public int SegmentLengthInSeconds { get; init; }
+}
